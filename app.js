8b7b2a3b807dd540b0a838a60bcc553167a178fa
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-12-30 23:03:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use('/steering-wheels', swRouter);
+app.use('/sw', swRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
