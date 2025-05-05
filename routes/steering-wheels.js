5705da411794cc962dfa7dc3868e4b8911c2dc9c
Olga Dzubak <iamAndriy80@gmail.com>
2025-05-05 16:19:27 +0300

    update

--- routes/steering-wheels.js
+++ routes/steering-wheels.js
@@ -3,6 +3,6 @@ const ctrl = require('../controllers/steering-wheels');
 const router = express.Router();
 
 router.get('/categories', ctrl.getCategories);
-router.get('/search', ctrl.getPhotos);
+router.get('/photos', ctrl.getPhotos);
 
 module.exports = router;
